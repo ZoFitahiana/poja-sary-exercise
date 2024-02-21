35cba8e19e7d3c9f9678c86adbe3c22c971c7ace
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-21 16:15:49 +0100

    feat : Create entity image and repository

---
+++ src/main/java/school/hei/sary/repository/ImageRepository.java
@@ -0,0 +1,12 @@
+package school.hei.sary.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import school.hei.sary.PojaGenerated;
+import school.hei.sary.repository.model.Image;
+@PojaGenerated
+@Repository
+
+public interface ImageRepository extends JpaRepository<Image, String> {
+}
---
+++ src/main/java/school/hei/sary/repository/model/Image.java
@@ -0,0 +1,17 @@
+package school.hei.sary.repository.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import school.hei.sary.PojaGenerated;
+
+@PojaGenerated
+@Entity
+@Getter
+@Setter
+
+public class Image {
+    @Id
+    private String id;
+}
